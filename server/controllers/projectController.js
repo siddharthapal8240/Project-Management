93f7d441e71ca18c9909ff97c02ca81aaf82f22d
Siddhartha Pal <siddhartha.pal.official@gmail.com>
2025-11-06 00:47:32 +0530

    Add member function add on the project controller

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -150,7 +150,7 @@ export const updateProject = async (req, res) => {
         end_date: end_date ? new Date(end_date) : null,
       },
     });
-    res.json({project, message: "Project updated successfully"})
+    res.json({ project, message: "Project updated successfully" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.code || error.message });
@@ -160,6 +160,45 @@ export const updateProject = async (req, res) => {
 // Add Member to Project
 export const addMember = async (req, res) => {
   try {
+    const { userId } = await req.auth();
+    const { projectId } = req.params;
+    const { email } = req.body;
+    // Check if user is project lead
+    const project = await prisma.project.findUnique({
+      where: { id: projectId },
+      include: { members: { include: { user: true } } },
+    });
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    if (project.team_lead !== userId) {
+      return res
+        .status(404)
+        .json({ message: "Only project lead can add members" });
+    }
+
+    // Check if user is already a member
+    const existingMember = project.members.find(
+      (member) => member.email === email
+    );
+    if (existingMember) {
+      return res.status(400).json({ message: "User is already a member" });
+    }
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const member = await prisma.projectMember.create({
+      data: {
+        userId: user.id,
+        projectId,
+      },
+    });
+
+    res.json({ member, message: "Member added successfully" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.code || error.message });
